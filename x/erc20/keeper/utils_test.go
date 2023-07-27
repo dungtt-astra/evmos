7335389be0b8c6f9e39804d5c5c34a741ed44648
dungtt-astra <dungtt@stellalab.com>
2023-07-27 08:59:29 +0700

    fixbug: testnet prefix

--- x/erc20/keeper/utils_test.go
+++ x/erc20/keeper/utils_test.go
@@ -75,7 +75,7 @@ func (suite *KeeperTestSuite) DoSetupTest(t require.TestingT) {
 	// init app
 	suite.app = app.Setup(false, feemarkettypes.DefaultGenesisState())
 	header := testutil.NewHeader(
-		1, time.Now().UTC(), "astra_11110-1", consAddress, nil, nil,
+		1, time.Now().UTC(), "astra_11115-1", consAddress, nil, nil,
 	)
 	suite.ctx = suite.app.BaseApp.NewContext(false, header)
 
